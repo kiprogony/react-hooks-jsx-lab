40cee6336b4540578b0b55b48e5fba7b1cd638c6
Daryl Rogony <darylrogony@gmail.com>
2022-05-04 14:31:11 +0300

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I am a senior Developer with over seven years of experience in Ruby on Rails</p>
+    <img
+      src={image} alt="I made this"
+      />
+  </div>
+  );
 }
 
 export default About;
